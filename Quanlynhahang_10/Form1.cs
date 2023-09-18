a1e86a4273e9e72716c308e6440177c1bcfbba62
nguyenchinhtan2003@gmail.com <nguyenchinhtan2003@gmail.com>
2023-09-19 06:58:51 +0700

    Add project files.

---
+++ Quanlynhahang_10/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Quanlynhahang_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowForm2()
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(ShowForm2)); //Tao luong moi
+            thread.Start();//khoi chay luong
+            this.Close();
+            
+        }
+    }
+}
---
+++ Quanlynhahang_10/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Quanlynhahang_10
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowForm1()
+        {
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+            Thread thread = new Thread(new ThreadStart(ShowForm1)); //Tao luong moi
+            thread.Start();//khoi chay luong
+            this.Close();
+
+            
+        }
+    }
+}
